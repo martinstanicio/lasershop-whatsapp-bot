49570fe06400784a25b465d5fc15fa201abb602f
Martín Stanicio <martuytimo@gmail.com>
2025-11-28 19:20:18 +0000

    load env variables and initialize twilio client

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
+from twilio.rest import Client
+
+load_dotenv()
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 
 app = Flask(__name__)
+twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 if __name__ == "__main__":
     app.run(port=3000)
